fe23898ec7f87f172861c1638a3b60f309edfee1
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-07-09 00:38:06 +0300

    add max operation

--- DStructures_2.0/main.cpp
+++ DStructures_2.0/main.cpp
@@ -8,7 +8,7 @@ struct BtNode {
     BtNode* right; // pointer που δειχνει στο δεξι παιδι
 };
 
-BtNode* GetNewNode(int data){
+BtNode* getNewNode(int data){
     BtNode* newNode = new BtNode(); //δεσμευουμε την μνημη για το νεο στοιχειο που εισαγουμε
     //θα μπορουοσαμε να χρησιμοποιήσουμε και την malloc εδω περα πχ, BtNode* newNode = (BtNode*)malloc(sizeof(BtNode));
     newNode->data = data; //εισαγουμε στην νεα δεσμευμενη μνημη τα δεδομενα της παραμετρου data
@@ -16,29 +16,39 @@ BtNode* GetNewNode(int data){
     return newNode;
 }
 
+
 //μεθοδος ευρεσης μεγιστου
-int FindMax(BtNode* rootPtr){
+void deleteMax(BtNode* maxNode){
 
 }
 
-//μεθοδος ευρεσης ελαχιστου
 //αυτο μπορει να γινει και με αναδρομη
-int FindMin(BtNode* rootPtr){ //για να βρουμε το ελαχιστο πρεπει να διασχιζουμε το δεντρο συνεχεια αριστερα
+BtNode* findMax(BtNode* rootPtr){ //για να βρουμε το μεγιστο πρεπει να διασχιζουμε το δεντρο συνεχεια δεξια
+    BtNode* current = rootPtr;
     if(rootPtr == NULL){ //εαν το δεντρο ειναι αδειο, επεστρεψε αυτο το error
         cout << "Error: Tree is empty\n";
-        return -1;
+        return  NULL;
     }
-    BtNode* current = rootPtr;
-    while(current->left != NULL){ //οσο αριστερα υπαρχει κομβος
-        current = current ->left; //αλλαξε τον pointer σε αυτον τον κομβο που βρεθηκε
+
+    BtNode* nextNode = current->right;
+    BtNode* max;
+    while(nextNode != NULL) { //οσο δεξια υπαρχει κομβος
+        if (nextNode->data >= current->data) { //αν το επομενο δεξιο παιδι ειναι μεγαλυτερο του current
+            max = nextNode; //ορισε για max το υποπαιδι
+        }
+        current = nextNode; //πηγαινε στο επομενα
+        nextNode = nextNode->right; //αλλαξε τον pointer σε αυτον τον κομβο που βρεθηκε
     }
-    return current->data;
+    return max ;
 }
 
+/*
+ * Πρεπει να ελεγχουμε ολοκληρο το υποδεντρο με την ριζα του και επειτα να εφαρμοζουμε την random για να τα πεταει μερικα στα δεξια παιδιαψ
+ * */
 //μεθοδος για εισαγωγη κομβων στο δεντρο
-BtNode* insert(BtNode* rootPtr, int data) {
+BtNode* insert(BtNode* rootPtr, int data){
     if(rootPtr == NULL){
-        rootPtr = GetNewNode(data);
+        rootPtr = getNewNode(data);
     }
     else if(data <= rootPtr->data){
         rootPtr->left = insert(rootPtr->left,data); //δημιουργια αριστερου παιδιου
@@ -50,12 +60,14 @@ BtNode* insert(BtNode* rootPtr, int data) {
 }
 
 //μεθοδος αναζητησης κομβων
-bool Search(BtNode* rootPtr, int data){
+bool search(BtNode *rootPtr, int data){
     if(rootPtr == NULL) return  false;
     else if(rootPtr->data == data) return true; // εαν το στοιχειο αντιστοιχει στην θεση μνημης, επεστρεψε true
-    else if(data <= rootPtr->data) return  Search(rootPtr->left,data); // εαν το στοιχειο ειναι μικροτερο απο το main στοιχειο, ψαξε το αριστερο παιδι
-    else return Search(rootPtr->right,data); // αλλιως αν ειναι μεγαλυτερο, ψαξε το δεξι παιδι
+    else if(data <= rootPtr->data) return search(rootPtr->left, data); // εαν το στοιχειο ειναι μικροτερο απο το main στοιχειο, ψαξε το αριστερο παιδι
+    else return search(rootPtr->right, data); // αλλιως αν ειναι μεγαλυτερο, ψαξε το δεξι παιδι
 }
+
+
 int main() {
     BtNode* rootPtr; // pointer που δειχνει στην ρίζα του δεντρου
     rootPtr = NULL; //αρχικοποιουμαι το δεντρο στο κενο
@@ -63,15 +75,16 @@ int main() {
     rootPtr = insert(rootPtr, 20);    rootPtr = insert(rootPtr,2);
     rootPtr = insert(rootPtr, 4);    rootPtr = insert(rootPtr, 100);
     rootPtr = insert(rootPtr, 10);    rootPtr = insert(rootPtr,245);
-    rootPtr = insert(rootPtr, 5);    rootPtr = insert(rootPtr, 1205);
+    rootPtr = insert(rootPtr, 5);    rootPtr = insert(rootPtr, 2);
 
 
 
-    int min = FindMin(rootPtr);
-    cout<<"The minimum is: " << min<<endl;
+    BtNode* maxNode = findMax(rootPtr);
+    deleteMax(maxNode);
+    cout<<"The maximum is: " << maxNode->data<<endl;
     int  number;
     cout<<"Enter the number you want to search in the binary tree"<<endl;
     cin>>number;
-    if(Search(rootPtr,number) == true) cout <<"found\n";
+    if(search(rootPtr, number) == true) cout <<"found\n";
     else cout<<"Not found\n";
 }
